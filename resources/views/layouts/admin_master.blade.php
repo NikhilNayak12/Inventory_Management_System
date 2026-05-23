62be0a4ed2799eef46d7136eafb3f57d64b08482
Nikhil07 <nikhil.nayak4561@gmail.com>
2026-05-23 12:47:09 +0530

    Use secure_asset in shared layouts

--- resources/views/layouts/admin_master.blade.php
+++ resources/views/layouts/admin_master.blade.php
@@ -8,7 +8,7 @@ Products<!DOCTYPE html>
         <meta name="author" content="" />
         <title>InventoryManagementSystem</title>        
 
-        <link href="{{ asset('backend') }}/css/styles.css" rel="stylesheet" />
+        <link href="{{ secure_asset('backend') }}/css/styles.css" rel="stylesheet" />
         <link href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" rel="stylesheet" crossorigin="anonymous" />
         <link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.7.0/css/buttons.dataTables.min.css">
         <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/js/all.min.js" crossorigin="anonymous"></script>
@@ -147,13 +147,13 @@ Products<!DOCTYPE html>
         </div>
         <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-        <script src="{{ asset('backend') }}/js/scripts.js"></script>
+        <script src="{{ secure_asset('backend') }}/js/scripts.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
-        <script src="{{ asset('backend') }}/assets/demo/chart-area-demo.js"></script>
-        <script src="{{ asset('backend') }}/assets/demo/chart-bar-demo.js"></script>
+        <script src="{{ secure_asset('backend') }}/assets/demo/chart-area-demo.js"></script>
+        <script src="{{ secure_asset('backend') }}/assets/demo/chart-bar-demo.js"></script>
         <!-- <script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js" crossorigin="anonymous"></script>
         <script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js" crossorigin="anonymous"></script> -->
-         <!-- <script src="{{ asset('backend') }}/assets/demo/datatables-demo.js"></script>  -->
+         <!-- <script src="{{ secure_asset('backend') }}/assets/demo/datatables-demo.js"></script>  -->
 <!-- 
         <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
         <script src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.min.js"></script>
